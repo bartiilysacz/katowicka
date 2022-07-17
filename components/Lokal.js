daefaf3e7f31bec3ef8daa28fef89ce20c875d2d
Piotr Dobrowolski <pdobrowolski@valuelogic.one>
2022-07-17 09:53:40 +0200

    Add changes

--- components/Lokal.js
+++ components/Lokal.js
@@ -20,7 +20,7 @@ const Lokal = ({ blok }) => {
       <Navbar />
       <div
         className="hero"
-        style={{ backgroundImage: `url(${blok.zdj_w_tle.filename}/m/${width}x0)` }}
+        style={{ backgroundImage: `url(${blok.zdj_w_tle.filename || blok.galeria[0].filename}/m/${width}x0)` }}
       >
         <div className="lokal-wrapper">
           <div className="left">
@@ -35,7 +35,7 @@ const Lokal = ({ blok }) => {
               {blok.galeria.map((item, index) => (
                 <div className="image-wrapper">
                   <a href={item.filename} key={index}>
-                    <img src={`${item.filename}/m/120x120`} alt="" />
+                    <img src={item.filename ? `${item.filename}/m/120x120` : } alt="" />
                   </a>
                 </div>
               ))}
