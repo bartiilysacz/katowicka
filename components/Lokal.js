99b8e14190b86b8fdcf42d3eaabcf2f96b0b4f8f
Piotr Dobrowolski <pdobrowolski@valuelogic.one>
2022-07-17 09:47:59 +0200

    Add changes

--- components/Lokal.js
+++ components/Lokal.js
@@ -1,5 +1,4 @@
-import React from "react";
-import ReactMarkdown from "react-markdown";
+import React, { useEffect, useState } from "react";
 import { SRLWrapper } from "simple-react-lightbox";
 import { sbEditable } from "@storyblok/storyblok-editable";
 import Navbar from "./custom/Navbar";
@@ -7,13 +6,21 @@ import Footer from "./custom/Footer";
 import Link from "next/link";
 
 const Lokal = ({ blok }) => {
+  const [width, setWidth] = useState(false);
+
+  useEffect(() => {
+    window && window.addEventListener('resize', ()=> {
+      setWidth(window.innerWidth)
+    })
+  }, [])
+
   console.log(blok);
   return (
     <div className="lokal" {...sbEditable(blok)}>
       <Navbar />
       <div
         className="hero"
-        style={{ backgroundImage: `url(${blok.galeria[0].filename})` }}
+        style={{ backgroundImage: `url(${blok.zdj_w_tle.filename}/m/${width}x0)` }}
       >
         <div className="lokal-wrapper">
           <div className="left">
@@ -28,7 +35,7 @@ const Lokal = ({ blok }) => {
               {blok.galeria.map((item, index) => (
                 <div className="image-wrapper">
                   <a href={item.filename} key={index}>
-                    <img src={item.filename} alt="" />
+                    <img src={`${item.filename}/m/120x120`} alt="" />
                   </a>
                 </div>
               ))}
